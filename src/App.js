5c66570110f1ff99ac0e28143915a7a354f4d6b6
AnnevanGorkom <anne@vangorkom.com>
2021-03-13 13:48:01 +0100

    Added API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import axios from "axios";
 import './App.css';
 
 function App() {
+
+  function handleResponse(response) {
+    // let country = response.data.data[0].country.displayName;
+    console.log(response.data);
+  }
+
+  function searchBrewery() {
+    let apiKey = "659d5c6b8f3d2447f090119e48202fdb";
+    let breweryParameter = "breweries";
+    let locationParameter = "withLocations=Y";
+    let apiUrl = `https://sandbox-api.brewerydb.com/v2/${breweryParameter}/?key=${apiKey}&${locationParameter}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  searchBrewery();
+
   return (
     <div className="App">
       <header className="App-header">
