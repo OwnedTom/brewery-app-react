df5bebc1f7fe86f9a28223c2bcf29f7ef0fb30a9
AnnevanGorkom <anne@vangorkom.com>
2021-03-14 22:16:14 +0100

    Update Brewery.js

--- src/components/Brewery.js
+++ src/components/Brewery.js
@@ -3,16 +3,21 @@ import React from "react";
 import '../style/Brewery.css';
 
 export default function Breweries(props) {
-  // creates the card with the brewery name, brewery description, and bwerery logo
+  // this is only to showcase how it would look. When this component is called by an iterating function, data[i] should take care of returning all the different brewery cards
+  let breweryName = props.data[0].name;
+  let description = props.data[0].description;
+  let image = props.data[0].image;
+
+  // creates the card with the brewery name, brewery description, and brewery logo
   return (
     <li className="Brewery">
       <div className="card">
           <div className="card-container">
-            <h4>{props.data.name}</h4>
-            <p>{props.data.description}</p>
+            <h4>{breweryName}</h4>
+            <p>{description}</p>
           </div>
           <div className="image">
-            <img src={props.data.image} alt={props.data.name} />
+            <img src={image} alt={breweryName} />
           </div>
         </div>
     </li>
