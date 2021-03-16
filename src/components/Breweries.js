3788feee420ff456ae4203deffbd24be02da5aeb
Tom Steele <tom@pxlwidgets.com>
2021-03-16 18:15:01 +0100

    Fixed issue

--- src/components/Breweries.js
+++ src/components/Breweries.js
@@ -1,38 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 import Brewery from "./Brewery";
 import '../style/Breweries.css';
 
+
 export default function Breweries() {
-  const breweryData = [];
+  const [breweryData, setBreweryData] = useState([]);
+  const [once, setOnce] = useState(false);
   
   // iterates through the API ressults to make objects of the breweries and adds them to an array
   function handleResponse(response) {
-    for (let i = 0; i < response.data.totalResults; i++) {
-      // gives a value to undefined properties
-      response.data.data[i].description = (typeof response.data.data[i].description !== "undefined") ? response.data.data[i].description: "no description available";
-      response.data.data[i].images = (typeof response.data.data[i].images !== "undefined") ? response.data.data[i].images: "No logo available";
-      response.data.data[i].locations = (typeof response.data.data[i].locations !== "undefined") ? response.data.data[i].locations: "No country available";
+    const breweries = response.data.data.map((data) => {
+      data.description = (typeof data.description !== "undefined") ? data.description: "no description available";
+      data.images = (typeof data.images !== "undefined") ? data.images: "No logo available";
+      data.locations = (typeof data.locations !== "undefined") ? data.locations: "No country available";
       
       // creates brewery object, more properties can be added
-      let brewery = {
-        name: response.data.data[i].name,
-        images: response.data.data[i].images,
-        description: response.data.data[i].description,
-        locations: response.data.data[i].locations
+      return {
+        name: data.name,
+        images: data.images,
+        description: data.description,
+        locations: data.locations
       };
-      
-      breweryData.push(brewery);
-    }
+    });
+    setBreweryData(breweries);
   }
 
   // calls the API
   function getBreweries() {
+    setOnce(true);
     let apiKey = "659d5c6b8f3d2447f090119e48202fdb";
     let breweryParameter = "breweries";
     let locationParameter = "withLocations=Y";
-    let apiUrl = `https://cors-anywhere.herokuapp.com/https://sandbox-api.brewerydb.com/v2/${breweryParameter}/?key=${apiKey}&${locationParameter}`;
+    let apiUrl = `http://localhost:5000/https://sandbox-api.brewerydb.com/v2/${breweryParameter}/?key=${apiKey}&${locationParameter}`;
     try {
       axios.get(apiUrl).then(handleResponse);
     } catch (error) {
@@ -40,12 +41,15 @@ export default function Breweries() {
     }
   }
 
+  if (! once) {
+    getBreweries();
+  }
+
   return (
     <div className="Breweries">
-      {getBreweries()}
       <ul>
         {/* needs to be changed to a function that iterates through the Brewery component */}
-        <Brewery data={breweryData} />
+        { breweryData.map((data, index) => <Brewery key={index} data={data} />) }
       </ul>
     </div>
   );
--- src/components/Brewery.js
+++ src/components/Brewery.js
@@ -5,9 +5,9 @@ import '../style/Brewery.css';
 export default function Breweries(props) {
   // this is only to showcase how it would look. When this component is called by an iterating function, data[i] should take care of returning all the different brewery cards
 
-  let breweryName = props.data[0].name;
-  let description = props.data[0].description;
-  let image = props.data[0].images.squareMedium;
+  let breweryName = props.data.name;
+  let description = props.data.description;
+  let image = props.data.images ? props.data.images.squareMedium : null;
 
   // creates the card with the brewery name, brewery description, and brewery logo
   return (
@@ -18,7 +18,7 @@ export default function Breweries(props) {
             <p>{description}</p>
           </div>
           <div className="image">
-            <img src={image} alt={breweryName} />
+            { image ? <img src={image} alt={breweryName} /> : 'No image available' }
           </div>
         </div>
     </li>
